c0dee546591465e296569e834190eaddc07fd763
vinit <vinitsxn@gmail.com>
2021-12-31 20:07:58 +0530

    Added: Welcome Message added

---
+++ src/main/java/addressBook/AddressBook.java
@@ -0,0 +1,14 @@
+/**
+ * @uthor: Vinit Kumar
+ * @created on: 31-Dec-2021
+ * Address Book
+ */
+package addressBook;
+
+public class AddressBook {
+    //main method
+    public static void main(String[] args) {
+        //welcome message
+        System.out.println("Welcome to Address Book Program!!!");
+    }
+}
